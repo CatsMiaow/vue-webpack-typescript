812b839df2c027cb8c386a961f4c775024c44ba9
CatsMiaow <infiltrator@naver.com>
2019-08-02 12:00:21 +0900

    Add icon font

--- src/vendor.ts
+++ src/vendor.ts
@@ -1,5 +1,6 @@
 // tslint:disable:no-import-side-effect
 
+import '@mdi/font/css/materialdesignicons.css';
 import 'material-design-icons-iconfont/dist/material-design-icons.css';
 import 'vuetify/dist/vuetify.min.css';
 
